ec967e3848d24187456e9c43a3f2f77f31a6d014
Pavel Korolev <p.koror56@gmail.com>
2025-10-31 21:12:56 +0500

    Made shorter and cleaner solution

--- ad-hoc/triangle.py
+++ ad-hoc/triangle.py
@@ -1,4 +1,4 @@
 # https://coderun.yandex.ru/problem/triangle
 
-nums = sorted(int(input()) for _ in range(3))
-print('YES' if nums[2] < nums[0] + nums[1] else 'NO')
+a, b, c = sorted(int(input()) for _ in range(3))
+print('YES' if a + b > c else 'NO')
